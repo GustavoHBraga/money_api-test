ac2bbe2f401390a7e7207b6c3750a6a4545c609d
GustavoHBraga <gh.brga@gmail.com>
2023-06-29 23:10:41 -0300

    first commit

---
+++ src/test/java/resource/CategoryTest.java
@@ -0,0 +1,73 @@
+package resource;
+
+import org.hamcrest.CoreMatchers;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+
+public class CategoryTest {
+	
+	@BeforeClass
+	public static void setup() {
+		RestAssured.baseURI = "http://localhost:8888/money-backend";
+	}
+	
+	public final String baseResource = "/category";
+	
+	@Test
+	public void testGetCategories() {
+		RestAssured
+			.given()
+				.log().all()
+			.when()
+				.get(baseResource)
+			.then()
+				.statusCode(200)
+				.contentType(ContentType.JSON)
+		;
+	}
+	@Test
+	public void testPostCategory() {
+		RestAssured
+			.given()
+				.body("{\"name\":\"Amazon-prime\"}")
+				.contentType(ContentType.JSON)
+			.when()
+				.post(baseResource)
+			.then()
+				.statusCode(201)
+				.header("Location",RestAssured.baseURI  + baseResource + "/14" )
+		;
+	}
+	@Test
+	public void testPostCategoryMessageError() {
+		RestAssured
+			.given()
+				.body("{\"name\":null}")
+				.contentType(ContentType.JSON)
+			.when()
+				.post(baseResource)
+			.then()
+				.statusCode(400)
+				.contentType(ContentType.JSON)
+				
+			;
+	}
+	@Test
+	public void testPostCategoryMessageErrorBylength() {
+		RestAssured
+			.given()
+				.body("{\"name\":\"A\"}")
+				.contentType(ContentType.JSON)
+			.when()
+				.post(baseResource)
+			.then()
+				.statusCode(400)
+				.contentType(ContentType.JSON)
+				
+			;
+	}
+	
+}
