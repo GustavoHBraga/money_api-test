47176cd9188e10abe2c207cc6db12371be25b1fa
GustavoHBraga <gh.brga@gmail.com>
2023-07-10 21:34:38 -0300

    remove peopleTests

--- src/test/java/resource/PeopleTest.java
+++ src/test/java/resource/PeopleTest.java
@@ -13,7 +13,7 @@ public class PeopleTest {
 		RestAssured.baseURI = "http://localhost:8006/money-backend";
 	}
 	
-	public final String baseResource = "/people";
+	public final String baseResource = "/person";
 	
 	@Test
 	public void testGetPeople() {
@@ -26,31 +26,5 @@ public class PeopleTest {
 				.statusCode(200);
 	}
 	
-	@Test
-	public void testCreatePeople() {
-		String peopleTest = "{\r\n"
-				+ "    \"name\": \"Gustavo Ninja gaiden 3\",\r\n"
-				+ "    \"informationAdress\": {\r\n"
-				+ "        \"adress\": \"Rua rio paranapanema\",\r\n"
-				+ "        \"number\": 147,\r\n"
-				+ "        \"cep\": \"1234-56\",\r\n"
-				+ "        \"district\": \"IAPI\",\r\n"
-				+ "        \"city\": \"Osasco\",\r\n"
-				+ "        \"state\": \"Sao Paulo\"\r\n"
-				+ "    },\r\n"
-				+ "    \"isActive\": true\r\n"
-				+ "}";
-		
-		RestAssured
-			.given()
-				.body(peopleTest)
-				.contentType(ContentType.JSON)
-			.when()
-				.post(baseResource)
-			.then()
-				.statusCode(201)
-				.header("Location",RestAssured.baseURI  + baseResource + "/1" )
-			;
-	}
 }
 
