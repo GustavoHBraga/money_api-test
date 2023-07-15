d659b4497c81cefccc3cb2ba199f24debc1577f0
GustavoHBraga <gh.brga@gmail.com>
2023-07-14 23:51:18 -0300

    add new test in PersonResoucet

--- src/test/java/resource/CategoryTest.java
+++ src/test/java/resource/CategoryTest.java
@@ -4,7 +4,6 @@ import static org.junit.Assert.assertEquals;
 
 import java.util.List;
 
-import org.hamcrest.CoreMatchers;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
---
+++ src/test/java/resource/PersonTest.java
@@ -0,0 +1,250 @@
+package resource;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+
+public class PersonTest {
+
+	@BeforeClass
+	public static void setup() {
+		RestAssured.baseURI = "http://localhost:8006/money-backend";
+	}
+	
+	public final String baseResource = "/person";
+	
+	@Test
+	public void testGetPeople() {
+		RestAssured
+			.given()
+				.log().all()
+			.when()
+				.get(baseResource)
+			.then()
+				.statusCode(200);
+	}
+	@Test
+	public void testPostPerson(){
+		String headerLocation = RestAssured.given()
+				.body("{\r\n"
+						+ "   \"name\": \"Gustavo H BRAGA\",\r\n"
+						+ "    \"active\": true,\r\n"
+						+ "    \"informationAdress\": {\r\n"
+						+ "        \"address\": \"Rua das palmeiras\",\r\n"
+						+ "        \"number\": 145,\r\n"
+						+ "        \"complement\": \"Apartamento 95\",\r\n"
+						+ "        \"district\": \"IAPO\",\r\n"
+						+ "        \"cep\": \"0000011\",\r\n"
+						+ "        \"city\": \"OSASCO\",\r\n"
+						+ "        \"state\": \"SAO PAULO\"\r\n"
+						+ "    }\r\n"
+						+ "}")
+				.contentType(ContentType.JSON)
+				.when()
+					.post(baseResource)
+				.then()
+					.statusCode(201)
+					.extract().header("Location")
+					;
+		
+		System.out.println(headerLocation);
+		
+		RestAssured.given()
+		.when()
+			.delete(headerLocation)
+		.then()
+			.statusCode(204);
+			
+	}
+	@Test
+	public void testPostPersonBadRequestActive() {
+		List<?> userMessage = RestAssured.given()
+				.body("{\r\n"
+						+ "   \"name\": \"Gustavo H BRAGA\",\r\n"
+						+ "    \"active\": null,\r\n"
+						+ "    \"informationAdress\": {\r\n"
+						+ "        \"address\": \"Rua das palmeiras\",\r\n"
+						+ "        \"number\": 145,\r\n"
+						+ "        \"complement\": \"Apartamento 95\",\r\n"
+						+ "        \"district\": \"IAPO\",\r\n"
+						+ "        \"cep\": \"0000011\",\r\n"
+						+ "        \"city\": \"OSASCO\",\r\n"
+						+ "        \"state\": \"SAO PAULO\"\r\n"
+						+ "    }\r\n"
+						+ "}")
+				.contentType(ContentType.JSON)
+				.when()
+					.post(baseResource)
+				.then()
+					.statusCode(400)
+					.extract().path("userMessage")
+					;
+		
+		assertEquals(userMessage.get(0), "Active is required");
+		
+			
+	}
+	@Test
+	public void testPostPersonBadRequestName() {
+		List<?> userMessage = RestAssured.given()
+				.body("{\r\n"
+						+ "   \"name\": null,\r\n"
+						+ "    \"active\": null,\r\n"
+						+ "    \"informationAdress\": {\r\n"
+						+ "        \"address\": \"Rua das palmeiras\",\r\n"
+						+ "        \"number\": 145,\r\n"
+						+ "        \"complement\": \"Apartamento 95\",\r\n"
+						+ "        \"district\": \"IAPO\",\r\n"
+						+ "        \"cep\": \"0000011\",\r\n"
+						+ "        \"city\": \"OSASCO\",\r\n"
+						+ "        \"state\": \"SAO PAULO\"\r\n"
+						+ "    }\r\n"
+						+ "}")
+				.contentType(ContentType.JSON)
+				.when()
+					.post(baseResource)
+				.then()
+					.statusCode(400)
+					.extract().path("userMessage")
+					;
+		
+		assertEquals(userMessage.get(0), "Name is required");
+	}
+	@Test
+	public void testUpdatePerson() {
+		String locationFistPerson = RestAssured.given()
+				.body("{\r\n"
+						+ "   \"name\": \"Gustavo H BRAGA\",\r\n"
+						+ "    \"active\": true,\r\n"
+						+ "    \"informationAdress\": {\r\n"
+						+ "        \"address\": \"Rua das palmeiras\",\r\n"
+						+ "        \"number\": 145,\r\n"
+						+ "        \"complement\": \"Apartamento 95\",\r\n"
+						+ "        \"district\": \"IAPO\",\r\n"
+						+ "        \"cep\": \"0000011\",\r\n"
+						+ "        \"city\": \"OSASCO\",\r\n"
+						+ "        \"state\": \"SAO PAULO\"\r\n"
+						+ "    }\r\n"
+						+ "}")
+				.contentType(ContentType.JSON)
+				.when()
+					.post(baseResource)
+				.then()
+					.statusCode(201)
+					.extract().header("Location")
+					;
+		
+		System.out.println(locationFistPerson);
+		
+		String nameUpdate = RestAssured.given()
+				.body("{\r\n"
+						+ "   \"name\": \"Gustavo Super Braga\",\r\n"
+						+ "    \"active\": true,\r\n"
+						+ "    \"informationAdress\": {\r\n"
+						+ "        \"address\": \"Rua das palmeiras\",\r\n"
+						+ "        \"number\": 145,\r\n"
+						+ "        \"complement\": \"Apartamento 95\",\r\n"
+						+ "        \"district\": \"IAPO\",\r\n"
+						+ "        \"cep\": \"0000011\",\r\n"
+						+ "        \"city\": \"OSASCO\",\r\n"
+						+ "        \"state\": \"SAO PAULO\"\r\n"
+						+ "    }\r\n"
+						+ "}")
+				.contentType(ContentType.JSON)
+				.when()
+					.put(locationFistPerson)
+				.then()
+					.statusCode(200)
+					.extract().path("name")
+					;
+		
+		assertEquals(nameUpdate, "Gustavo Super Braga");
+	}
+	@Test
+	public void testUpdateActivePerson() {
+		String locationFistPerson = RestAssured.given()
+				.body("{\r\n"
+						+ "   \"name\": \"Gustavo H BRAGA\",\r\n"
+						+ "    \"active\": true,\r\n"
+						+ "    \"informationAdress\": {\r\n"
+						+ "        \"address\": \"Rua das palmeiras\",\r\n"
+						+ "        \"number\": 145,\r\n"
+						+ "        \"complement\": \"Apartamento 95\",\r\n"
+						+ "        \"district\": \"IAPO\",\r\n"
+						+ "        \"cep\": \"0000011\",\r\n"
+						+ "        \"city\": \"OSASCO\",\r\n"
+						+ "        \"state\": \"SAO PAULO\"\r\n"
+						+ "    }\r\n"
+						+ "}")
+				.contentType(ContentType.JSON)
+				.when()
+					.post(baseResource)
+				.then()
+					.statusCode(201)
+					.extract().header("Location")
+					;
+		
+		
+		boolean activeUpdate = RestAssured.given()
+				.body("false")
+				.contentType(ContentType.JSON)
+				.when()
+					.put(locationFistPerson + "/active")
+				.then()
+					.statusCode(200)
+					.extract().path("active")
+					;
+		
+		assertEquals(activeUpdate, false);
+	}
+	@Test
+	public void testUpdateAddressPerson() {
+		String locationFistPerson = RestAssured.given()
+				.body("{\r\n"
+						+ "   \"name\": \"Gustavo H BRAGA\",\r\n"
+						+ "    \"active\": true,\r\n"
+						+ "    \"informationAdress\": {\r\n"
+						+ "        \"address\": \"Rua das palmeiras\",\r\n"
+						+ "        \"number\": 145,\r\n"
+						+ "        \"complement\": \"Apartamento 95\",\r\n"
+						+ "        \"district\": \"IAPO\",\r\n"
+						+ "        \"cep\": \"0000011\",\r\n"
+						+ "        \"city\": \"OSASCO\",\r\n"
+						+ "        \"state\": \"SAO PAULO\"\r\n"
+						+ "    }\r\n"
+						+ "}")
+				.contentType(ContentType.JSON)
+				.when()
+					.post(baseResource)
+				.then()
+					.statusCode(201)
+					.extract().header("Location")
+					;
+		
+		System.out.println(locationFistPerson);
+		String address = RestAssured.given()
+				.body("{\r\n"
+						+ "    \"address\": \"Chaves e encanto\",\r\n"
+						+ "    \"number\": 555,\r\n"
+						+ "    \"cep\": \"1895-6\",\r\n"
+						+ "    \"city\": \"Osasco\",\r\n"
+						+ "    \"district\": \"IAPI\",\r\n"
+						+ "    \"state\": \"Sao Paulo\"\r\n"
+						+ "}")
+				.contentType(ContentType.JSON)
+				.when()
+					.put(locationFistPerson + "/address")
+				.then()
+					.statusCode(200)
+					.extract().path("informationAdress.address")
+					;
+		assertEquals(address, "Chaves e encanto");
+	}
+}
+
